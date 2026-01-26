c8ab04140e5bbbf7f8fe4617aba17bf0dfffa879
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2026-01-27 02:44:09 +0400

    Finish general front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,33 @@ import Features from "../components/home/features";
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-white font-sans text-gray-700 relative">
-      <section className="relative w-full h-[500px] flex items-center justify-center">
+      <section
+        className="
+    relative
+    w-full
+    h-[420px]
+    sm:h-[450px]
+    md:h-[480px]
+    lg:h-[520px]
+    overflow-hidden
+  "
+      >
         <Image
           src={Hero}
           alt="Student Accommodation"
-          className="absolute top-0 left-0 z-0 w-full h-full"
+          fill
+          priority
+          sizes="100vw"
+          className="
+      absolute inset-0 z-0
+      object-cover
+      object-right
+    "
         />
 
-        <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-10"></div>
+        <div className="absolute inset-0 z-10 bg-black/50" />
 
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white z-20 px-4">
+        <div className="pt-16 md:p-0 absolute inset-0 flex flex-col items-center justify-center text-center text-white z-20 px-4">
           <h1 className="text-4xl md:text-5xl font-bold mb-4">
             Discover the Best Student Accommodation
           </h1>
@@ -30,20 +47,20 @@ export default function HomePage() {
             the globe
           </p>
 
-          <div className="flex flex-row gap-4 justify-center items-center">
-            <div className="flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
+          <div className="flex flex-col md:flex-row gap-4 justify-center items-center w-full max-w-3xl">
+            <div className="justify-center w-full flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
               <CircleCheckBig className="h-4 w-4" />
 
               <p>Verified Listings</p>
             </div>
 
-            <div className="flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
+            <div className="justify-center w-full flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
               <Headset className="h-4 w-4" />
 
               <p>24x7 Assistance</p>
             </div>
 
-            <div className="flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
+            <div className="justify-center w-full flex items-center flex-row gap-2 py-2 px-3 bg-black/30 rounded-lg text-white">
               <Banknote className="h-4 w-4" />
 
               <p>Lowest Price Guarantee</p>
